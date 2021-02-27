dd785adb15dcdb00c94e2de6611f79034363288d
moazzamsyntax <moazzamsyntax@gmail.com>
2021-02-27 12:40:24 -0500

    9

---
+++ src/test/java/com/hrms/utils/apiConstants.java
@@ -0,0 +1,16 @@
+package com.hrms.utils;
+
+import io.restassured.RestAssured;
+
+public class apiConstants {
+
+    public static final String BaseURI= RestAssured.baseURI="http://3.237.189.167/syntaxapi/api";
+    public static final String GENERATE_TOKEN_URI=BaseURI+"/generateToken.php";
+    public static final String CREATE_EMPLOYEE_URI=BaseURI+"/createEmployee.php";
+    public static final String GET_ONE_EMPLOYEE_URI=BaseURI+"/getOneEmployee.php";
+    public static final String UPDATE_EMPLOYEE_URI=BaseURI+"/updateEmployee.php";
+    public static final String GET_ALL_EMPLOYEE_URI=BaseURI+"/getAllEmployees.php";
+    public static final String DELETE__EMPLOYEE_URI=BaseURI+"//deleteEmployee.php";
+
+
+}
